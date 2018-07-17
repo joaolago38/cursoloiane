9b67eea9fadce6034c0cc48b5e62428c22cde769
joaolago38 <joaolago35@gmail.com>
2018-07-17 10:38:39 -0300

    comit de aulas loiane

--- src/com/loiane/cursojava/aula12/LeituraDadosTeclado.java
+++ src/com/loiane/cursojava/aula12/LeituraDadosTeclado.java
@@ -1,10 +1,40 @@
 package com.loiane.cursojava.aula12;
 
+import java.util.Scanner;
+
 public class LeituraDadosTeclado {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+//		System.out.println("Digite seu nome completo:");
+//		 String nomeCompleto = scan.nextLine();
+//		 System.out.println("Seu nome completo é: " + nomeCompleto);
+//		 System.out.println("=====================================");
+//		 System.out.println("Digite seu primeiro nome:");
+//		 String primeiroNome = scan.next();
+//		 System.out.println("=====================================");
+//		 System.out.println("Digite a sua idade:");
+//		 int idade = scan.nextInt();
+//		 System.out.println("Sua idade é: " + idade + " anos");
+//		 System.out.println("=====================================");
+//		 System.out.println("Digite a sua altura:");
+//		 double altura = scan.nextDouble();
+//		 System.out.println("A sua altura é: " + altura + " cm");
+//		 System.out.println("=====================================");
 
+		System.out.println("Digite o seu primeiro nome, idade, quantidade de filhos, altura e se tem bichinho de estimação:");
+		String primeiroNome = scan.next();
+		int idade = scan.nextInt();
+		byte qtdFilhos = scan.nextByte();
+		float altura = scan.nextFloat();
+//		boolean temPet = scan.nextBoolean();
+		
+		System.out.println("Você digitou os seguintes valores:");
+		System.out.println("Primeiro nome: " + primeiroNome);
+		System.out.println("Idade: " + idade);
+		System.out.println("Quantidade de filhos: " + qtdFilhos);
+		System.out.println("Altura: " + altura);
+//		System.out.println("Tem bichinho de estimação? " + temPet);
 	}
 
 }
---
+++ src/com/loiane/cursojava/aula13/labs/Exer10.java
@@ -0,0 +1,19 @@
+package com.loiane.cursojava.aula13.labs;
+
+import java.util.Scanner;
+
+public class Exer10 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Entre com a temperatura em Celsius:");
+		double c = scan.nextDouble();
+
+		double f = (c * 1.8) + 32;
+
+		System.out.println("A temperatura " + c + " C é igual a " + f + " F");
+
+	}
+
+}
